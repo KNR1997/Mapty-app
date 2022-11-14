92017d07432c870fc3259ba1071001631a4e30d6
kethaka <kethaka.ranasinghe@itechro.com>
2022-11-14 12:58:17 +0530

    display a map using leaflet library

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+
+if (navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+            const {latitude} = position.coords;
+            const {longitude} = position.coords;
+            console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+            const cords = [latitude, longitude];
+
+            const map = L.map('map').setView(cords, 13);
+
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            L.marker(cords).addTo(map)
+                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+                .openPopup();
+        }, 
+        function() {
+            alert('Could not get your location');
+        }   
+    );
